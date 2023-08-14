692437586c2881c77fda2e07a14ce188c952cb4d
Ib-Rey <ib-rey@yandex.ru>
2023-08-14 11:49:40 +0300

    added settings, import, ai_settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,13 @@ import sys
 #Модуль sys завершает игру по команде игрока
 
 import pygame
+from settings import Settings
 def run_game():
     #Инициализирует игру и создает объект экрана
     pygame.init()
-    screen = pygame.display.set_mode((1000, 600))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     #Назначение цвета фона
     bg_color = (230, 230, 230)
@@ -20,7 +23,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             #При каждом проходе цикла перерисовывается экран
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
         #Отображение последнего прорисованного экрана
         pygame.display.flip()
 run_game()        
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    #Класс для храения настроек игры
+
+    def __init__(self):
+        '''Инициализирует настройки игры'''
+        #Параметры экрана
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230,230)
+
