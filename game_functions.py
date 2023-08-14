7801b5ea63f75afdadeb2f480ad18057953cf147
Ib-Rey <ib-rey@yandex.ru>
2023-08-14 14:24:40 +0300

    рефакторинг,создан game_functions, класс chek_events,

---
+++ game_functions.py
@@ -0,0 +1,8 @@
+import sys
+import pygame
+
+def check_events():
+    '''Обработка нажатия клавиш и события мыши'''
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            sys.exit()
