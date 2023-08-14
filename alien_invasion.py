661c8919f3696f643020b317f1438aff98f946e7
Ib-Rey <ib-rey@yandex.ru>
2023-08-14 10:28:08 +0300

    gray screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 600))
     pygame.display.set_caption("Alien Invasion")
+    #Назначение цвета фона
+    bg_color = (230, 230, 230)
 
     
     
@@ -17,6 +19,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            #При каждом проходе цикла перерисовывается экран
+            screen.fill(bg_color)
         #Отображение последнего прорисованного экрана
         pygame.display.flip()
 run_game()        
