550adee0736c94d76cb0d98b9b312eed0ff80b2c
Ib-Rey <ib-rey@yandex.ru>
2023-08-26 13:30:45 +0300

    умеьшение размера корабля

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings():
     def __init__(self):
         '''Инициализирует настройки игры'''
         #Параметры экрана
-        self.screen_width = 1000
-        self.screen_height = 600
+        self.screen_width = 1100
+        self.screen_height = 700
         self.bg_color = (58, 58, 125)
 
--- ship.py
+++ ship.py
@@ -6,7 +6,7 @@ class Ship():
         #Инициализирует корабль и задает первонач. позицию
         self.screen = screen
         #Загрузка изображения корабля и получение прямоугольника
-        self.image = pygame.image.load('images/scuter.png')
+        self.image = pygame.image.load('images/scuter1.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         #Каждый новый корабль появляется у нижнего края экрана
