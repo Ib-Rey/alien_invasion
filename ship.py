2c5b965c4aedebfcca3f549370f7beecdb00f1a8
Ib-Rey <ib-rey@yandex.ru>
2023-08-29 19:56:05 +0300

    движется в обе стороны

--- ship.py
+++ ship.py
@@ -12,12 +12,15 @@ class Ship():
         #Каждый новый корабль появляется у нижнего края экрана
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        # Флаг перемещения
+        # Флаги перемещения
         self.moving_right = False
+        self.moving_left = False
     def update(self):
         '''Обновляет позицию корабля с учетом флага'''
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         #Рисует корабль в текущей позиции
