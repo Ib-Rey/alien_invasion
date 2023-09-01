2ec9e61ed1ebe327fc11c0f181025fd3c255de73
Vita <ibrey@yandex.ru>
2023-09-01 10:33:54 +0300

    границы экрана

--- ship.py
+++ ship.py
@@ -21,10 +21,10 @@ class Ship():
     def update(self):
         '''Обновляет позицию корабля с учетом флага'''
         #Обновляет атрибут center, не rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
             
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
         #Обновление атрибута rect на основе self.center
