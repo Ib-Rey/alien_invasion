15cef226eb3b6bf2dac2055c3a94f4301caf1137
Vita <ibrey@yandex.ru>
2023-11-05 19:21:11 +0300

    aliens moving

--- game_functions.py
+++ game_functions.py
@@ -102,9 +102,23 @@ def get_number_rows(ai_settings, ship_height, alien_height):
                             (3*alien_height) - ship_height)
     number_rows = int(available_space_y / (2*alien_height))
     return number_rows
-def update_aliens(aliens):
-    #Обновляет позиции всех пришельцев на флоте
+def update_aliens(ai_settings, aliens):
+    #Проверяет достиг ли флот края экрана после чего
+    # Обновляет позиции всех пришельцев на флоте
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    #Реагирует на достижение пришельцем края экрана
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+def change_fleet_direction(ai_settings, aliens):
+    #Опускает флот и меняет направление флота
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
 
         
            
--- settings.py
+++ settings.py
@@ -14,8 +14,9 @@ class Settings():
         self.bullet_color = 237, 214, 88
         self.bullets_allowed = 3
         #Настройки кораблz
-        ship_speed_factor = 1.5
+        self.ship_speed_factor = 1.5
         #Настройка пришельцев
         self.alien_speed_factor = 1
-
-
+        self.fleet_drop_speed = 10
+        # fleet direction = 1 движение вправо,а  -1 - влево
+        self.fleet_direction = 1
