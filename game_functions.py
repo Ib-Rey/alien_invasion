05ae9a1a773b79df59d90573411d3282cf848142
Vita <ibrey@yandex.ru>
2023-11-05 16:45:14 +0300

    refactoring game function

--- game_functions.py
+++ game_functions.py
@@ -66,18 +66,29 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 
 def create_fleet(ai_settings, screen, aliens):
     #Создает флот пришельцев
+    
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2*alien_width
-    number_aliens_x = int(available_space_x / (2*alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    
     #Создание первого ряда пришельцев
     for alien_number in range(number_aliens_x):
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2*alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
+       
+        
 
+def get_number_aliens_x(ai_settings, alien_width):
+    #Вычисляет количество пришельцев в ряду
+    available_space_x = ai_settings.screen_width - 2*alien_width
+    number_aliens_x = int(available_space_x / (2*alien_width))
+    return number_aliens_x
 
+def create_alien(ai_settings, screen, aliens, alien_number):
+    #Создает пришельца и размещает его в ряду
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2*alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
         
            
 
